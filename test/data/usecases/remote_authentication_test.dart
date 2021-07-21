36fcce4c698c3cd2b6f2dfbb2e00b90d368bda40
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-07-20 23:56:05 -0300

    refactor(RemoteAuthentication): move sut creation to setUp method

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,20 +1,26 @@
 import 'package:faker/faker.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:moovbr/data/http/custom_http_client.dart';
 import 'package:test/test.dart';
 
+import 'package:moovbr/data/http/custom_http_client.dart';
 import 'package:moovbr/data/usecases/remote_authentication.dart';
 
 import 'remote_authentication_test.mocks.dart';
 
 @GenerateMocks([CustomHttpClient])
 void main() {
-  test('Should call CustomHttpClient with correct values', () async {
-    final httpClient = MockCustomHttpClient();
-    final url = faker.internet.httpUrl();
-    final sut = RemoteAuthentication(httpClient, url);
+  CustomHttpClient httpClient = MockCustomHttpClient();
+  String url = faker.internet.httpUrl();
+  RemoteAuthentication sut = RemoteAuthentication(httpClient, url);
 
+  setUp(() {
+    httpClient = MockCustomHttpClient();
+    url = faker.internet.httpUrl();
+    sut = RemoteAuthentication(httpClient, url);
+  });
+
+  test('Should call CustomHttpClient with correct values', () async {
     await sut.auth();
 
     verify(httpClient.request(url, 'POST'));
