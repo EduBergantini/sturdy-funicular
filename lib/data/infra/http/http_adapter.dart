fd1761a056169f7a496556cae489c24b5cdbb991
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-07-23 00:26:13 -0300

    feat(HttpAdapter): ensure it calls post with correct header

--- lib/data/infra/http/http_adapter.dart
+++ lib/data/infra/http/http_adapter.dart
@@ -2,11 +2,16 @@ import 'package:http/http.dart';
 
 class HttpAdapter {
   final Client httpClient;
+  final Map<String, String> customHeaders = {
+    'User-Agent': 'MoovBR.Mobile/1.0.0',
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  };
 
   HttpAdapter(this.httpClient);
 
   Future<void> request(String url, String method) async {
-    await this.httpClient.post(Uri.parse(url));
+    await this.httpClient.post(Uri.parse(url), headers: customHeaders);
     return;
   }
 }
