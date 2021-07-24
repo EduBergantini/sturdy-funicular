1e33f7d739c050d118163816f104a8a561589414
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-07-24 12:14:02 -0300

    feat(HttpAdapter): ensure it returns null when post returns 200 with no data

--- lib/data/infra/http/http_adapter.dart
+++ lib/data/infra/http/http_adapter.dart
@@ -18,6 +18,8 @@ class HttpAdapter implements CustomHttpClient {
     final response = await this.httpClient.post(Uri.parse(url),
         headers: customHeaders, body: body != null ? jsonEncode(body) : null);
 
+    if (response.body.isEmpty) return null;
+
     return jsonDecode(response.body);
   }
 }
